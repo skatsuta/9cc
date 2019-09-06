4760efa3ccb9780f3b84c09f1759c2395edd50d4
Soshi Katsuta <soshi.katsuta@gmail.com>
2019-09-06 23:23:38 +0900

    test: add test.sh

---
+++ test.sh
@@ -0,0 +1,27 @@
+#!/usr/bin/env bash
+set -u
+
+CC=gcc
+BIN=9cc
+TMP=tmp
+
+try() {
+  local expected="$1"
+  local input="$2"
+
+  ./${BIN} "${input}" > ${TMP}.s
+  ${CC} -o ${TMP} ${TMP}.s
+  ./${TMP}
+  local actual="$?"
+
+  if [[ "${actual}" != "${expected}" ]]; then
+    echo "${input} => ${expected} expected, but got ${actual}"
+    exit 1
+  fi
+  echo "${input} => ${actual}"
+}
+
+try 0 0
+try 42 42
+
+echo "OK"
